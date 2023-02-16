9db9a820ee18aabb42a32cf1ff563a780d1ecc0a
ultra <77009494+0x-ultra@users.noreply.github.com>
2023-02-16 12:03:29 +0100

    Update IViewer.sol

--- src/interfaces/IViewer.sol
+++ src/interfaces/IViewer.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.10;
 
 interface IViewer {
-    function getGlpRatioWithoutFees(uint256 _amount) external view returns (uint256);
+    function getGlpRatioWithoutRetention(uint256 _amount) external view returns (uint256);
     function getUSDCRatio(uint256 _jUSDC) external view returns (uint256);
 }
